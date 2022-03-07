d3f596f62975a526f52782ae0e027f59c065d863
Kang <1823515579@qq.com>
2022-03-07 18:10:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.git;
+
+/**
+ * @author kang
+ * @date 2022/3/7-18:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+        System.out.println("Hello git2!");
+        System.out.println("Hello git3!");
+        System.out.println("Hello git4!");
+        System.out.println("master test");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+    }
+}
